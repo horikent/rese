ed5a5c3c0e71da723adc7760ee86b33b5d7c750b
horikent <horikent@gmail.com>
2022-09-28 09:00:26 +0900

    2nd-commit

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,18 +1,101 @@
+
+
+<style>
+
+
+a{
+  text-decoration: none;
+  color: blue
+}
+.nav{
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  left: -100%;
+  background: #eee;
+  transition: .7s;
+  text-align: center;
+}
+.nav ul{
+  padding-top: 80px;
+}
+.nav ul li{
+  list-style-type: none;
+  margin-top: 50px;
+}
+.menu {
+  display: inline-block;
+  width: 36px;
+  height: 32px;
+  cursor: pointer;
+  position: relative;
+  left: 20px;
+  top: 20px;
+}
+.menu__line--top,
+.menu__line--middle,
+.menu__line--bottom {
+  display: inline-block;
+  width: 100%;
+  height: 4px;
+  background-color: #000;
+  position: absolute;
+  transition: 0.5s;
+}
+.menu__line--top {
+  top: 0;
+}
+.menu__line--middle {
+  top: 14px;
+}
+.menu__line--bottom {
+  bottom: 0;
+}
+.menu.open span:nth-of-type(1) {
+  top: 14px;
+  transform: rotate(45deg);
+}
+.menu.open span:nth-of-type(2) {
+  opacity: 0;
+}
+.menu.open span:nth-of-type(3) {
+  top: 14px;
+  transform: rotate(-45deg);
+}
+.in{
+  transform: translateX(100%);
+}
+
+</style>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Rese</title>
 
-<h2>rese</h2>
+
 </head>
 <body>
+  <div class="header__container">
+  <nav class="nav" id="nav">
+    <ul>
+      <li><a href="#">リンク1</a></li>
+      <li><a href="#">リンク2</a></li>
+      <li><a href="#">リンク3</a></li>
+    </ul>
+  </nav>
+  <div class="menu" id="menu">
+    <span class="menu__line--top"></span>
+    <span class="menu__line--middle"></span>
+    <span class="menu__line--bottom"></span>
+  </div>
+  <h2>rese</h2>
   <div class="detail__container">
     <div class="detail__item">
       <h2>{{$shop->name}}</h2>
-      <img src="{{$shop->image}}" alt="">
+      <img src="{{asset($shop->image)}}" alt="">
       <p>##{{$shop->area}}&emsp;##{{$shop->genre}}</p>
       <p>{{$shop->detail}}</p><br>
     </div>
@@ -26,5 +109,6 @@
         </form>
     </div>
   </div>
+  <script src="{{ asset('/js/rese.js') }}"></script>
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,69 @@
   margin-top: 30px;
 }
 
+a{
+  text-decoration: none;
+  color: blue
+}
+.nav{
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+  left: -100%;
+  background: #eee;
+  transition: .7s;
+  text-align: center;
+}
+.nav ul{
+  padding-top: 80px;
+}
+.nav ul li{
+  list-style-type: none;
+  margin-top: 50px;
+}
+.menu {
+  display: inline-block;
+  width: 36px;
+  height: 32px;
+  cursor: pointer;
+  position: relative;
+  left: 20px;
+  top: 20px;
+}
+.menu__line--top,
+.menu__line--middle,
+.menu__line--bottom {
+  display: inline-block;
+  width: 100%;
+  height: 4px;
+  background-color: #000;
+  position: absolute;
+  transition: 0.5s;
+}
+.menu__line--top {
+  top: 0;
+}
+.menu__line--middle {
+  top: 14px;
+}
+.menu__line--bottom {
+  bottom: 0;
+}
+.menu.open span:nth-of-type(1) {
+  top: 14px;
+  transform: rotate(45deg);
+}
+.menu.open span:nth-of-type(2) {
+  opacity: 0;
+}
+.menu.open span:nth-of-type(3) {
+  top: 14px;
+  transform: rotate(-45deg);
+}
+.in{
+  transform: translateX(100%);
+}
+
 </style>
 <!DOCTYPE html>
 <html lang="ja">
@@ -23,20 +86,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rese</title>
 </head>
-<header class="shop__header">
-  <div class="header__container">
-    <h2>Rese</h2>
-    <div class=search__var></div>
-      <input type="hidden" name="area">
-        <p>All area</p>
-      <input type="hidden" name="genre">
-        <p>All genre</p>
-      <input type="text">
-    </div>
-  </div>  
-</header>
-
 <body>
+  <header class="shop__header">
+    <div class="header__container">
+      <nav class="nav" id="nav">
+        <ul>
+          <li><a href="#">リンク1</a></li>
+          <li><a href="#">リンク2</a></li>
+          <li><a href="#">リンク3</a></li>
+        </ul>
+      </nav>
+      <div class="menu" id="menu">
+        <span class="menu__line--top"></span>
+        <span class="menu__line--middle"></span>
+        <span class="menu__line--bottom"></span>
+      </div>
+      <h2>Rese</h2>
+      <div class=search__var></div>
+        <input type="hidden" name="area">
+          <p>All area</p>
+        <input type="hidden" name="genre">
+          <p>All genre</p>
+        <input type="text">
+      </div>
+    </div>  
+  </header>
   <div class="shop__container">
     <div class="item">
       @foreach($shops as $shop)
@@ -48,5 +122,6 @@
         @endforeach
     </div>
   </div>
+  <script src="{{ asset('/js/rese.js') }}"></script>
 </body>
 </html>
