2b4305f365c34fceadf6a638a4d489ac05cc600a
horikent <horikent@gmail.com>
2022-11-04 23:30:15 +0900

    27th-commit

--- database/migrations/2022_09_25_221524_create_shops_table.php
+++ database/migrations/2022_09_25_221524_create_shops_table.php
@@ -20,11 +20,9 @@ return new class extends Migration
             $table->foreignId('genre_id');
             $table->text('detail');
             $table->text('image');
-            $table->string('name')->comment('店舗名');
             $table->timestamp('created_at')->useCurrent()->nullable();
             $table->timestamp('updated_at')->useCurrent()->nullable();
         });
-
     }
 
     /**
